c53fd235aa4cc3babd478733b3c0abdfc718eb32
Thomas Brier <tombrier@hotmail.co.uk>
2021-11-05 02:29:06 +0000

    Patching more holes in the logic parser

--- logic_evaluator.py
+++ logic_evaluator.py
@@ -17,15 +17,14 @@ env = {
     'multiply' : {'symbol' : '*', 'value' : operator.__mul__, 'nargs' : 2},
     'divide' : {'symbol' : '//', 'value' : operator.__truediv__, 'nargs' : 2},
     'power' : {'symbol' : '^', 'value' : operator.__pow__, 'nargs' : 2},
+    'mod' : {'symbol' : '%', 'value' : operator.__mod__, 'nargs' : 2},
     'not' : {'symbol' : '~', 'value' : operator.__not__, 'nargs' : 1},
-    'neg' : {'symbol' : '-', 'value' : operator.__neg__, 'nargs' : 1},
+    'negate' : {'symbol' : '-', 'value' : operator.__neg__, 'nargs' : 1},
     'T' : {'symbol' : 'T', 'value' : True, 'nargs' : 0},
     'F' : {'symbol' : 'F', 'value' : False, 'nargs' : 0},
 }
 
 def get_tokens(user_input):
-    if user_input.count('(') != user_input.count(')'):
-        raise LogicEvaluatorError('incorrect syntax')
     user_input = user_input.replace(' ', '')
     for k in env.keys():
         user_input = user_input.replace(env[k]['symbol'], f" {env[k]['symbol']} ")        
@@ -34,67 +33,86 @@ def get_tokens(user_input):
     tokens = user_input.split()
     return tokens
 
-def get_expression(tokens):
-    num_tokens = len(tokens)
+def get_expression(tokens, sub_expr_count=0):
     i = 0
     expression = []
-    if num_tokens == 0:
-        raise LogicEvaluatorError('unexpected end of expression')
-    if tokens[0] == ')':
-        raise LogicEvaluatorError('incorrect syntax')
-    else:
-        while i < num_tokens:
-            if tokens[i] == '(':
-                sub_expression, sub_i = get_expression(tokens[i+1:])
-                i += sub_i + 1
-                expression.append(sub_expression)
-            elif tokens[i] != ')':
-                in_env = False
-                for k in env.keys():
-                    if tokens[i] == env[k]['symbol']:
-                        expression.append(tokens[i])
-                        in_env = True
-                        break
-                if not in_env:
-                    try:
-                        env[tokens[i]] = {'symbol' : tokens[i], 'value' : int(tokens[i]), 'nargs' : 0}
+    if len(tokens) == 0:
+        raise LogicEvaluatorError("Syntax error: Expression ended before closing '('")            
+    if tokens[0] == ')' and sub_expr_count == 0:
+        raise LogicEvaluatorError("Syntax error: Encountered closing ')' without opening '('")
+    elif tokens[0] == ')' and sub_expr_count > 0:
+        raise LogicEvaluatorError("Syntax error: Encountered closing ')' instantly after opening '('")
+    while i < len(tokens):
+        if tokens[i] == ')' and sub_expr_count > 0:
+            sub_expr_count -= 1
+            return expression, i, sub_expr_count
+        elif tokens[i] == '(':
+            sub_expr_count += 1
+            sub_expression, sub_i, sub_expr_count = get_expression(tokens[i+1:], sub_expr_count=sub_expr_count)
+            i += sub_i + 1
+            expression.append(sub_expression)
+        elif tokens[i] != '(' and tokens[i] != ')':
+            in_env = False
+            for k in env.keys():
+                if tokens[i] == env[k]['symbol']:
+                    expression.append(k)
+                    in_env = True
+                    break
+            if not in_env:
+                try:
+                    env[tokens[i]] = {'symbol' : tokens[i], 'value' : int(tokens[i]), 'nargs' : 0}
+                    expression.append(tokens[i])
+                except ValueError:
+                    try: 
+                        env[tokens[i]] = {'symbol' : tokens[i], 'value' : float(tokens[i]), 'nargs' : 0}
                         expression.append(tokens[i])
                     except ValueError:
-                        try: 
-                            env[tokens[i]] = {'symbol' : tokens[i], 'value' : float(tokens[i]), 'nargs' : 0}
-                            expression.append(tokens[i])
-                        except ValueError:
-                            env[tokens[i]] = {'symbol' : tokens[i], 'value' : str(tokens[i]), 'nargs' : 0}
-                            expression.append(tokens[i])
-                i += 1                
-            else:
-                i += 1
-                break
-    return expression, i
+                        env[tokens[i]] = {'symbol' : tokens[i], 'value' : str(tokens[i]), 'nargs' : 0}
+                        expression.append(tokens[i])
+        i += 1    
+    if tokens.count('(') < tokens.count(')'):
+        raise LogicEvaluatorError("Syntax error: Missing opening '('")
+    elif tokens.count('(') > tokens.count(')'):
+        raise LogicEvaluatorError("Syntax error: Missing closing ')'")
+    return expression, i, sub_expr_count
+        
 
 def evaluate(expr, accept_type_errors=False):
     try:
         if not isinstance(expr, list):
-            for k in env.keys():
-                if expr == env[k]['symbol']:
-                    return env[k]['value']
+            if env[expr]['nargs'] == 0:
+                return env[expr]['value']
+            else:
+                raise LogicEvaluatorError(f'Impossible use of operator: {env[expr]["symbol"]}')
         else:
             if len(expr) == 1:
                 return evaluate(expr[0])
             elif len(expr) == 2:
-                for k in env.keys():
-                    if expr[0] == env[k]['symbol'] and env[k]['nargs'] == 1:
-                        return env[k]['value'](evaluate(expr[1]))
+                if expr[0] == 'subtract':
+                    # edge case where two different operations share the same symbol
+                    expr[0] = 'negate'             
+                if env[expr[0]]['nargs'] == 0:
+                    raise LogicEvaluatorError(f'Impossible use of value: {env[expr[0]]["symbol"]}') 
+                elif env[expr[0]]['nargs'] == 1:
+                    return env[expr[0]]['value'](evaluate(expr[1]))
+                elif env[expr[0]]['nargs'] == 2:
+                    raise LogicEvaluatorError(f'Impossible use of operator: {env[expr[0]]["symbol"]}')
             elif len(expr) == 3:
-                for k in env.keys():
-                    if expr[1] == env[k]['symbol'] and env[k]['nargs'] == 2:
-                        return env[k]['value'](evaluate(expr[0]), evaluate(expr[2]))    
-    except TypeError as e:
+                if env[expr[1]]['nargs'] == 0:
+                    raise LogicEvaluatorError(f'Impossible use of value: {env[expr[1]]["symbol"]}') 
+                elif env[expr[1]]['nargs'] == 1:
+                    raise LogicEvaluatorError(f'Impossible use of operator: {env[expr[1]]["symbol"]}') 
+                elif env[expr[1]]['nargs'] == 2:
+                    return env[expr[1]]['value'](evaluate(expr[0]), evaluate(expr[2]))
+            else:
+                raise LogicEvaluatorError(f'Ambiguous or invalid expression: {" ".join([env[k]["symbol"] for k in expr])}')
+    except TypeError:
         if not accept_type_errors:
             raise LogicEvaluatorError('Attempted impossible comparison')
         else:
             return None
-    raise LogicEvaluatorError('Incorrect syntax')
+    except ZeroDivisionError:
+        raise LogicEvaluatorError('Attempted division by zero')
 
 def interpret(user_input, user_env=None, accept_type_errors=False):
     if user_env is not None:
