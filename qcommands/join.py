d6cda3388f2a54a96e1f8eb215e3f225ac6906f7
Thomas Brier <tombrier@hotmail.co.uk>
2021-11-09 01:11:53 +0000

    Updated join command

--- qcommands/join.py
+++ qcommands/join.py
@@ -1,3 +1,4 @@
+import re
 import functools
 from quantum_state import QuantumState
 
@@ -8,21 +9,35 @@ def command(env, command_args):
     name_tag = 'name='
     states_being_joined = []
     states_to_pop = []
-    joint_state_name = ""
+    joint_state_name = None
     for s in command_args:
-        if s.startswith(name_tag):
-            joint_state_name = s[len(name_tag):]
+        if '=' in s:
+            if s.startswith(name_tag):
+                tag_value = s[len(name_tag):]
+                # If the joint name contains an illegal character, declare an error
+                illegal_chars = re.search("[^0-9a-zA-Z]", tag_value)
+                if illegal_chars:
+                    raise JoinCommandError(f"Invalid character(s) in joint state name: {tag_value[illegal_chars.span()[0]]}")
+                else:
+                    joint_state_name = tag_value
+            else:
+                # We assume a faulty tag assignment and raise appropriate error
+                raise JoinCommandError(f"'{s.split('=')[0]}' is not a recognised tag.")
         elif s in env['states_dict'] and s not in states_to_pop:
             states_being_joined.append(env['states_dict'][s])
             states_to_pop.append(s)
         elif s in states_to_pop:
-            raise JoinCommandError(f"attempted joining of state {s} with itself")  
+            raise JoinCommandError(f"Attempted joining of state {s} with itself.")  
         else:
-            raise JoinCommandError(f"invalid parameter assignment or state that doesn't exist: {s}")
+            raise JoinCommandError(f"State '{s}' doesn't exist.")
+    # Remove states that are being joined together from the environment
     for s in states_to_pop:
-        env['states_dict'].pop(s)                                 
+        env['states_dict'].pop(s)                    
+    # Create joint state by 'multiplying' states together
     joint_state = functools.reduce(QuantumState.__mul__, states_being_joined)
+    # If a valid name was provided, assign it
     if joint_state_name:
         joint_state.state_name = joint_state_name
+    # Add the joint state to the environment and return the environment
     env['states_dict'][joint_state.state_name] = joint_state
     return env
