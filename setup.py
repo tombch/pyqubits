88c38d8edd95971b449f1ad60750e64a27f64751
tombch <t.brier@outlook.com>
2023-10-17 00:59:23 +0100

    Bump to fix bug with publish failing due to markdown description

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ import setuptools
 setuptools.setup(
     name="pyqubits",
     author="Thomas Brier",
-    version="0.2.0",
+    version="0.2.1",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy",
